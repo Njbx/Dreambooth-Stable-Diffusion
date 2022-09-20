8bcba80d2ecd098b0100def5ee142cb5ebe982e3
Njbx <80281506+Njbx@users.noreply.github.com>
2022-09-20 20:01:16 +0200

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import PIL
+from pathlib import Path
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
@@ -8,129 +9,11 @@ from torchvision import transforms
 import random
 
 training_templates_smallest = [
-    'photo of a sks {}',
+    'a {} {}',
 ]
 
 reg_templates_smallest = [
-    'photo of a {}',
-]
-
-imagenet_templates_small = [
-    'a photo of a {}',
-    'a rendering of a {}',
-    'a cropped photo of the {}',
-    'the photo of a {}',
-    'a photo of a clean {}',
-    'a photo of a dirty {}',
-    'a dark photo of the {}',
-    'a photo of my {}',
-    'a photo of the cool {}',
-    'a close-up photo of a {}',
-    'a bright photo of the {}',
-    'a cropped photo of a {}',
-    'a photo of the {}',
-    'a good photo of the {}',
-    'a photo of one {}',
-    'a close-up photo of the {}',
-    'a rendition of the {}',
-    'a photo of the clean {}',
-    'a rendition of a {}',
-    'a photo of a nice {}',
-    'a good photo of a {}',
-    'a photo of the nice {}',
-    'a photo of the small {}',
-    'a photo of the weird {}',
-    'a photo of the large {}',
-    'a photo of a cool {}',
-    'a photo of a small {}',
-    'an illustration of a {}',
-    'a rendering of a {}',
-    'a cropped photo of the {}',
-    'the photo of a {}',
-    'an illustration of a clean {}',
-    'an illustration of a dirty {}',
-    'a dark photo of the {}',
-    'an illustration of my {}',
-    'an illustration of the cool {}',
-    'a close-up photo of a {}',
-    'a bright photo of the {}',
-    'a cropped photo of a {}',
-    'an illustration of the {}',
-    'a good photo of the {}',
-    'an illustration of one {}',
-    'a close-up photo of the {}',
-    'a rendition of the {}',
-    'an illustration of the clean {}',
-    'a rendition of a {}',
-    'an illustration of a nice {}',
-    'a good photo of a {}',
-    'an illustration of the nice {}',
-    'an illustration of the small {}',
-    'an illustration of the weird {}',
-    'an illustration of the large {}',
-    'an illustration of a cool {}',
-    'an illustration of a small {}',
-    'a depiction of a {}',
-    'a rendering of a {}',
-    'a cropped photo of the {}',
-    'the photo of a {}',
-    'a depiction of a clean {}',
-    'a depiction of a dirty {}',
-    'a dark photo of the {}',
-    'a depiction of my {}',
-    'a depiction of the cool {}',
-    'a close-up photo of a {}',
-    'a bright photo of the {}',
-    'a cropped photo of a {}',
-    'a depiction of the {}',
-    'a good photo of the {}',
-    'a depiction of one {}',
-    'a close-up photo of the {}',
-    'a rendition of the {}',
-    'a depiction of the clean {}',
-    'a rendition of a {}',
-    'a depiction of a nice {}',
-    'a good photo of a {}',
-    'a depiction of the nice {}',
-    'a depiction of the small {}',
-    'a depiction of the weird {}',
-    'a depiction of the large {}',
-    'a depiction of a cool {}',
-    'a depiction of a small {}',
-]
-
-imagenet_dual_templates_small = [
-    'a photo of a {} with {}',
-    'a rendering of a {} with {}',
-    'a cropped photo of the {} with {}',
-    'the photo of a {} with {}',
-    'a photo of a clean {} with {}',
-    'a photo of a dirty {} with {}',
-    'a dark photo of the {} with {}',
-    'a photo of my {} with {}',
-    'a photo of the cool {} with {}',
-    'a close-up photo of a {} with {}',
-    'a bright photo of the {} with {}',
-    'a cropped photo of a {} with {}',
-    'a photo of the {} with {}',
-    'a good photo of the {} with {}',
-    'a photo of one {} with {}',
-    'a close-up photo of the {} with {}',
-    'a rendition of the {} with {}',
-    'a photo of the clean {} with {}',
-    'a rendition of a {} with {}',
-    'a photo of a nice {} with {}',
-    'a good photo of a {} with {}',
-    'a photo of the nice {} with {}',
-    'a photo of the small {} with {}',
-    'a photo of the weird {} with {}',
-    'a photo of the large {} with {}',
-    'a photo of a cool {} with {}',
-    'a photo of a small {} with {}',
-]
-
-per_img_token_list = [
-    'א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י', 'כ', 'ל', 'מ', 'נ', 'ס', 'ע', 'פ', 'צ', 'ק', 'ר', 'ש', 'ת',
+    'a {}',
 ]
 
 class PersonalizedBase(Dataset):
@@ -189,13 +72,17 @@ class PersonalizedBase(Dataset):
 
         if not image.mode == "RGB":
             image = image.convert("RGB")
-
+            
         placeholder_string = self.placeholder_token
+        pathname = Path(self.image_paths[i % self.num_images]).name
+        placeholder_token = pathname.split("_")[0]
+        
         if self.coarse_class_text:
             placeholder_string = f"{self.coarse_class_text} {placeholder_string}"
-
+        
         if not self.reg:
-            text = random.choice(training_templates_smallest).format(placeholder_string)
+            #You only need to use 1 template for Dreambooth, but you can try more if you wished (not recommended)
+            text = random.choice(training_templates_smallest).format(placeholder_token, placeholder_string)
         else:
             text = random.choice(reg_templates_smallest).format(placeholder_string)
             
